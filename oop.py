fb73d896d11155f871de4d218d2b2a757d4fefdb
AnVu1708 <121224169+AnVu1708@users.noreply.github.com>
2023-01-03 18:00:28 +0700

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,3 +1,4 @@
+#class, contrusctor, abilities 
 class animal:
     def __init__(self, height,heavy,sex,move): 
         self.height = height
@@ -10,3 +11,21 @@ Lion.type = "Lion"
 Bird = animal("25cm","10kg","male"," are flying")
 Bird.type = "Bird"
 print(Lion.sex + " " +Lion.type + Lion.move) 
+
+#Instance method
+class animal:
+    name = "Bird"
+    def color(self):
+        print ("Red")
+pinkbird = animal()
+pinkbird.name = pinkbird
+pinkbird.color()
+Carolasparotia = animal()
+Carolasparotia.name = "Carolasparotia"
+Carolasparotia.color()
+class animal:
+    color = ""
+pinkbird = animal()
+pinkbird.color= "pink"
+Carolasparotia = animal ()
+Carolasparotia.color ="red"
